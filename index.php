9d029c525d013bf9afcf95381a01af8e8020ee67
root <root@vps397571.ovh.net>
2017-06-28 15:10:22 +0200

    material zoning

--- index.php
+++ index.php
@@ -1,64 +1,87 @@
-<audio id="audio" style="display:none" v-model="player" src="src/bip.mp3"></audio>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<div class="container">
-  <div id="app" class="row">
-    <div class="col-md-12">
-      <h2>Chrono Planning V0.1</h2>
-    </div>
-    <div class="col-md-12">
-      <ul class="list-group">
-        <li v-for="chrono in chronos" v-bind:class="chrono.color" class="list-group-item">
-          <span class="badge">{{chrono.duration}}</span>
-          {{chrono.name}}
-        </li>
-      </ul>
-    </div>
-    <div class="col-md-12">
-			<form class="">
-			 <h3 v-if="currentChrono">{{currentChrono.name}} - {{currentChrono.duration}} secondes / {{time}} secondes </h3>
-       <div class="progress">
-          <div class="progress-bar" role="progressbar" v-bind:style="'width:'+ passed_percent +'%'" v-bind:aria-valuenow="passed_percent" aria-valuemin="0" aria-valuemax="100"></div>
-       </div>
-       <button type="button" v-on:click="play()" class="btn btn-success">Play</button>
-       <button type="button" v-on:click="pause_time()" class="btn btn-warning">Pause</button>
-       <button type="button" v-on:click="stop()" class="btn btn-danger">Stop</button>
-			</form>
-    </div>
-		<div class="col-md-12">
-      <form class="">
-        <div class="form-group">
-          <label class="sr-only" for="name">Nom du chrono</label>
-          <input type="text" v-model="form.name" class="form-control" id="name" placeholder="Nom du chrono">
-        </div>
-        <div class="form-group">
-          <label class="sr-only" for="duration">Durée en seconde</label>
-          <input type="number" v-model="form.duration" pattern="[0-9]"  class="form-control" id="duration" placeholder="Durée en seconde">
-        </div>
-        <div class="checkbox">
-          <label>
-            <input v-model="form.color" value="success" type="radio"> Green
-          </label>
-          <label>
-            <input v-model="form.color" value="danger" type="radio"> Red
-          </label>
-          <label>
-            <input v-model="form.color" value="info" type="radio"> Blue
-          </label>          
-          <label>
-            <input v-model="form.color" value="warning" type="radio"> Orange
-          </label>
-        </div>
-        <button type="button" v-on:click="addChrono(form.name,form.duration,form.color)" class="btn btn-default">Add a Step/Chrono</button>
-      </form>
-    </div>
-  </div>
-</div>
 
-<script
-			  src="https://code.jquery.com/jquery-3.2.1.min.js"
-			  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-			  crossorigin="anonymous">
-</script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/vue"></script>
-<script src="js/app.js"></script>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+	
+	<!-- Add to homescreen for Chrome on Android -->
+	<meta name="mobile-web-app-capable" content="yes">
+
+	<!-- Add to homescreen for Safari on iOS -->
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="apple-mobile-web-app-title" content="Material Design Lite">
+
+	<title>Chrono Planning v1.0</title>
+</head>
+<body class="">
+	<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+		<header class="mdl-layout__header">
+			<div class="mdl-layout__header-row">
+				<span class="mdl-layout-title">Chrono Planning v1.0</span>
+			</div>
+  	</header>
+		<main id="app" class="mdl-layout__content">
+			<div class="page-content">
+				<div class="mdl-grid">
+					<div class="mdl-cell mdl-cell--12-col">
+						<ul class="mdl-list">
+							<h4 class="mdl-cell mdl-cell--12-col"> Chronos </h4>
+							<table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+								<tbody>
+									<tr>
+										<td class="mdl-data-table__cell--non-numeric"><i class="material-icons  mdl-list__item-alarm">alarm</i></td>
+										<td>pompes</td>
+										<td>20 secondes</td>
+									</tr>
+								</tbody>
+							</table>
+						</ul>
+					</div>
+					<div class="mdl-cell mdl-cell--12-col mdl-cell_timer">
+						<h3>00:00 / 00:00</h3> 
+					</div>
+					<div class="mdl-cell mdl-cell--12-col mdl-cell_action">
+						<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+							<i class="material-icons">play_arrow</i>
+						</button>
+						<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+							<i class="material-icons">pause</i>
+						</button>
+						<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+							<i class="material-icons">stop</i>
+						</button>
+					</div>
+					<div class="mdl-cell mdl-cell--12-col">
+						<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button flt-right">
+							<i class="material-icons">alarm_add</i>
+						</button>
+					</div>
+				</div>
+			</div>
+		</main>
+		<footer>
+			<!-- 	Audio biper -->
+			<audio id="audio" style="display:none" v-model="player" src="audio/bip.mp3"></audio>
+
+			<!-- 	Material Design Lite version -->
+			<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+			<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+			<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-light_blue.min.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+			<!-- 	VueJs & App -->
+			<script src="https://unpkg.com/vue"></script>
+			<script src="js/app.js"></script>
+		</footer>
+	</div>
+</body>
+<footer>
+
+</footer>
+</html>
+
