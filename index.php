940bae3c597a45d711c6709db6850b33bdb6e76d
Alaric LEGRAIS <root@vps397571.ovh.net>
2017-07-04 18:38:22 +0200

    thx dialog-polyfill

--- index.php
+++ index.php
@@ -12,12 +12,13 @@
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-status-bar-style" content="black">
 	<meta name="apple-mobile-web-app-title" content="Material Design Lite">
-
+  <link rel="stylesheet" type="text/css" href="node_modules/dialog-polyfill/dialog-polyfill.css" />
 	<title>Chrono Planning v1.0</title>
 </head>
 <body>
 	<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
 		<header class="mdl-layout__header">
+			
 			<div class="mdl-layout__header-row">
 				<span class="mdl-layout-title">Chrono Planning v1.0</span>
 			</div>
@@ -106,6 +107,8 @@
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
 	<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-light_blue.min.css" />
 	<link rel="stylesheet" href="css/style.css" />
+	
+  <script src="node_modules/dialog-polyfill/dialog-polyfill.js"></script>
 	<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 	
 	<!-- 	VueJs & App -->
